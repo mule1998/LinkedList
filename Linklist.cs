e140c6360f1ef3a7761d29edcee5b53713ef2bbf
Shubham Mule <shubhambmule@gmail.com>
2021-10-24 15:36:09 +0530

    Adding : Middle Insertion

--- Linklist.cs
+++ Linklist.cs
@@ -8,7 +8,6 @@ namespace DataStructureDemo
 {
     class CustomLinkedList
     {
-     
         Node head;
 
         public void InsertLast(int new_data)
@@ -49,6 +48,25 @@ namespace DataStructureDemo
             InsertLast(new_data);
         }
 
+        public void InsertMid(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            Node pos = head;
+            int len = 0;
+            while (pos != null)
+            {
+                len++;
+                pos = pos.next;
+            }
+            int count = ((len % 2) == 0) ? (len / 2) : (len + 1) / 2;
+            pos = head;
+            while (count-- > 1)
+            {
+                pos = pos.next;
+            }
+            new_node.next = pos.next;
+            pos.next = new_node;
+        }
         public void Display()
         {
             Console.WriteLine("Displaying Nodes");
